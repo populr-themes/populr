5eebcbf5943522cd12318efece8ec8a29c5d13c0
Jim Cipriani <jimcipriani@gmail.com>
2013-04-10 11:37:37 -0500

    Update Populr theme to use new image slider system

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 $(document).on('pop-initialized', function(){
- 
  	
-  $('.asset').live('initialize', function(e, asset){
+  // $('.asset').live('initialize', function(e, asset){
 
-    $('.asset-type-imagegroup .slideshow .images').each(function() {
-    	var $this = $(this);
-    	$this.cycle({
-				fx: 'fade',
-				timeout: Math.floor(Math.random() * 8000) + 3000,
-				delay: Math.floor(Math.random() * 6000) + 1000
-	    });
-		});
+  //   $('.asset-type-imagegroup .slideshow .images').each(function() {
+  //   	var $this = $(this);
+  //   	$this.cycle({
+		// 		fx: 'fade',
+		// 		timeout: Math.floor(Math.random() * 8000) + 3000,
+		// 		delay: Math.floor(Math.random() * 6000) + 1000
+	 //    });
+		// });
 
-  });
+  // });
 
 
   $('.asset').live('destroy', function(e, asset){
